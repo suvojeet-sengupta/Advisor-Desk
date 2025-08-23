b8a037f605b589fb13990b86ad90cf6bf94e0620
suvojit213 <suvojitsengupta21@gmail.com>
2025-08-23 23:27:55 +0530

    fix: Make AdService.showAd return Future<void> to allow awaiting

--- lib/data/datasources/ad_service.dart
+++ lib/data/datasources/ad_service.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdService {
@@ -19,20 +20,29 @@ class AdService {
     );
   }
 
-  void showAd() {
+  Future<void> showAd() {
     if (_interstitialAd != null) {
+      final completer = Completer<void>();
       _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
         onAdDismissedFullScreenContent: (ad) {
           ad.dispose();
           loadAd();
+          if (!completer.isCompleted) {
+            completer.complete();
+          }
         },
         onAdFailedToShowFullScreenContent: (ad, error) {
           ad.dispose();
           loadAd();
+          if (!completer.isCompleted) {
+            completer.completeError(error); // Or just complete() if you don't want to propagate the error
+          }
         },
       );
       _interstitialAd!.show();
       _interstitialAd = null;
+      return completer.future;
     }
+    return Future.value(); // Return a completed future if no ad is shown
   }
 }
